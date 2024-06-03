de38d418c703c3b031d62d6319383537ecd423e2
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-06-03 17:25:30 +0100

    Create base files for all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
-
+import Navbar from "./components/Navbar.jsx";
+import About from "./components/About.jsx";
+import Projects from "./components/Projects.jsx";
+import Skills from "./components/Skills.jsx";
+import Contact from "./components/Contact.jsx";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <div></div>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
     </div>
   );
 }
